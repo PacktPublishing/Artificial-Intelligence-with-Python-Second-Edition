3f59db8540c365f9341176acd4511a451f8c7511
alberto33 <alberto33@gmail.com>
2020-01-12 15:51:44 -0500

    Delete __init__.py

--- AI/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Negamax import Negamax
-from .TT import TT
-from .solving import id_solve, df_solve
-from .MTdriver import mtd
-from .SSS import SSS
-from .DUAL import DUAL
-from .HashTT import HashTT
