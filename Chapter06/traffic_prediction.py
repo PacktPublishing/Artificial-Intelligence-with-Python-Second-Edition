4c5d6eefb7e2e1a932fc68388b667b18418da925
alberto33 <alberto33@gmail.com>
2020-01-27 16:46:09 -0500

    update code to handle deprecated package

--- Chapter06/traffic_prediction.py
+++ Chapter06/traffic_prediction.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, mean_absolute_error
-from sklearn import cross_validation, preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import classification_report
 
@@ -29,7 +30,7 @@ X = X_encoded[:, :-1].astype(int)
 y = X_encoded[:, -1].astype(int)
 
 # Split data into training and testing datasets 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25, random_state=5)
 
 # Extremely Random Forests regressor
@@ -44,16 +45,6 @@ print("Mean absolute error:", round(mean_absolute_error(y_test, y_pred), 2))
 # Testing encoding on single data instance
 test_datapoint = ['Saturday', '10:20', 'Atlanta', 'no']
 test_datapoint_encoded = [-1] * len(test_datapoint)
-count = 0
-for i, item in enumerate(test_datapoint):
-    if item.isdigit():
-        test_datapoint_encoded[i] = int(test_datapoint[i])
-    else:
-        test_datapoint_encoded[i] = int(label_encoder[count].transform(test_datapoint[i]))
-        count = count + 1 
-
-test_datapoint_encoded = np.array(test_datapoint_encoded)
 
 # Predict the output for the test datapoint
 print("Predicted traffic:", int(regressor.predict([test_datapoint_encoded])[0]))
-
