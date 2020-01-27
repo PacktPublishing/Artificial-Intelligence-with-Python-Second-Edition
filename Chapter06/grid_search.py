3406db121ecc1ae46e47f8056693f0650041f5b1
alberto33 <alberto33@gmail.com>
2020-01-27 16:38:44 -0500

    updated depracated code

--- Chapter06/grid_search.py
+++ Chapter06/grid_search.py
@@ -1,12 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
-from sklearn import cross_validation, grid_search
+#from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn import cross_validation
 from sklearn.metrics import classification_report
 
-from utilities import visualize_classifier
+# Define the minimum and maximum values for X and Y
+# that will be used in the mesh grid
+def visualize_classifier(classifier, X, y):
+  min_x, max_x = X[:, 0].min() - 1.0, X[:, 0].max() + 1.0
+  min_y, max_y = X[:, 1].min() - 1.0, X[:, 1].max() + 1.0
+
+print("\nLoad file")
 
 # Load input data
 input_file = 'data_random_forests.txt'
@@ -19,7 +27,7 @@ class_1 = np.array(X[y==1])
 class_2 = np.array(X[y==2])
 
 # Split the data into training and testing datasets 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25, random_state=5)
 
 # Define the parameter grid 
@@ -32,18 +40,17 @@ metrics = ['precision_weighted', 'recall_weighted']
 for metric in metrics:
     print("\n##### Searching optimal parameters for", metric)
 
-    classifier = grid_search.GridSearchCV(
+    classifier = GridSearchCV(
             ExtraTreesClassifier(random_state=0), 
             parameter_grid, cv=5, scoring=metric)
     classifier.fit(X_train, y_train)
-
+    
     print("\nGrid scores for the parameter grid:")
-    for params, avg_score, _ in classifier.grid_scores_:
-        print(params, '-->', round(avg_score, 3))
+    for results in classifier.cv_results_:
+        print(results)
 
     print("\nBest parameters:", classifier.best_params_)
 
     y_pred = classifier.predict(X_test)
     print("\nPerformance report:\n")
     print(classification_report(y_test, y_pred))
-
