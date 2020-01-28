965f6427b5ea16d23a1d2e2cf38d2df12fe9aa07
alberto33 <alberto33@gmail.com>
2020-01-28 14:34:37 -0500

    make sure to use "pip install logic" to install logpy

--- Chapter09/prime.py
+++ Chapter09/prime.py
@@ -1,3 +1,6 @@
+# to install logpy use:
+# pip install logic
+
 import itertools as it
 import logpy.core as lc
 from sympy.ntheory.generate import prime, isprime
@@ -12,11 +15,6 @@ def check_prime(x):
 # Declate the variable
 x = lc.var()
 
-# Check if an element in the list is a prime number
-list_nums = (23, 4, 27, 17, 13, 10, 21, 29, 3, 32, 11, 19)
-print('\nList of primes in the list:')
-print(set(lc.run(0, x, (lc.membero, x, list_nums), (check_prime, x))))
-
 # Print first 7 prime numbers
 print('\nList of first 7 prime numbers:')
-print(lc.run(7, x, check_prime(x)))
+print(lc.run(20, x, check_prime(x)))
