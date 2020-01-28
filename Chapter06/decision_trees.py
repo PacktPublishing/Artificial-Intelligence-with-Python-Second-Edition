dbfe03624650b240767f554c506ebd19892b1376
alberto33 <alberto33@gmail.com>
2020-01-28 18:55:54 -0500

    update package import

--- Chapter06/decision_trees.py
+++ Chapter06/decision_trees.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
-from sklearn import cross_validation
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 from utilities import visualize_classifier
@@ -24,7 +24,7 @@ plt.scatter(class_1[:, 0], class_1[:, 1], s=75, facecolors='white',
 plt.title('Input data')
 
 # Split data into training and testing datasets 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+X_train, X_test, y_train, y_test = train_test_split.train_test_split(
         X, y, test_size=0.25, random_state=5)
 
 # Decision Trees classifier 
