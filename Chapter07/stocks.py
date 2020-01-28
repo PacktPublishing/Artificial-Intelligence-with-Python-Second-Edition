f15060320347783a6497f9535681f9e6e92ff7a1
alberto33 <alberto33@gmail.com>
2020-01-28 14:23:40 -0500

    update to use library that is not obsolete

--- Chapter07/stocks.py
+++ Chapter07/stocks.py
@@ -4,7 +4,9 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import covariance, cluster
-from matplotlib.finance import quotes_historical_yahoo_ochl as quotes_yahoo
+
+import yfinance as yf
+
 
 # Input file containing company symbols 
 input_file = 'company_symbol_mapping.json'
@@ -16,14 +18,14 @@ with open(input_file, 'r') as f:
 symbols, names = np.array(list(company_symbols_map.items())).T
 
 # Load the historical stock quotes 
-start_date = datetime.datetime(2003, 7, 3)
-end_date = datetime.datetime(2007, 5, 4)
-quotes = [quotes_yahoo(symbol, start_date, end_date, asobject=True) 
+start_date = datetime.datetime(2019, 1, 1)
+end_date = datetime.datetime(2019, 1, 31)
+quotes = [yf.Ticker(symbol).history(start=start_date, end=end_date) 
                 for symbol in symbols]
 
 # Extract opening and closing quotes
-opening_quotes = np.array([quote.open for quote in quotes]).astype(np.float)
-closing_quotes = np.array([quote.close for quote in quotes]).astype(np.float)
+opening_quotes = np.array([quote.Open for quote in quotes]).astype(np.float)
+closing_quotes = np.array([quote.Close for quote in quotes]).astype(np.float)
 
 # Compute differences between opening and closing quotes 
 quotes_diff = closing_quotes - opening_quotes
@@ -47,4 +49,3 @@ num_labels = labels.max()
 print('\nClustering of stocks based on difference in opening and closing quotes:\n')
 for i in range(num_labels + 1):
     print("Cluster", i+1, "==>", ', '.join(names[labels == i]))
-
